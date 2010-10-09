7140b2c9a7723dff209fcc6056f30806aa19fdb5
ma.schneidt <ma.schneidt@561f5f3c-573f-c28a-3d4a-dce8d887f103>
2010-10-09 16:36:42 +0000

    -cleared mem prevented loading bouquets more then once

--- trunk/dboxtv_neutrino/dboxTV_neutrino.dpr
+++ trunk/dboxtv_neutrino/dboxTV_neutrino.dpr
@@ -1249,12 +1249,6 @@ try
   Result := iInxBouquet;
 
 finally
-  // free mem
-  if Assigned(RegExServices) then
-    RegExServices.Free;
-    
-  if Assigned(RegExChannels) then
-    RegExChannels.Free;
 
   Bouquets.Free;
   Services.Free;
